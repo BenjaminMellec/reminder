47f83c9d6936147b797ceea6fbc7edb05f82aa7c
Benjamin <benjamin.mellec@yousign.fr>
2018-06-03 20:57:04 +0200

    update slideshows

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,45 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndex = [1,1];
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+function plusSlides(n, no) {
+    showSlides(slideIndex[no] += n, no);
 }
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
+function showSlides(n, no) {
+    var i;
+    var x = document.getElementsByClassName(slideId[no]);
+    if (n > x.length) {slideIndex[no] = 1}
+    if (n < 1) {slideIndex[no] = x.length}
+    for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
+    }
+    x[slideIndex[no]-1].style.display = "block";
 }
 
 
 $('#wireframe').addClass('reminder__nav-tab--current');
 $('#wireframe, #mockup, #tools').click(function() {
-	$('#wireframe, #mockup, #tools').removeClass('reminder__nav-tab--current')
-	$(this).addClass('reminder__nav-tab--current');
+    $('#wireframe, #mockup, #tools').removeClass('reminder__nav-tab--current')
+    $(this).addClass('reminder__nav-tab--current');
+});
+
+
+$('#wireframe').click(function(){
+    $('#js-wireframe-slide').addClass('show').removeClass('hide')
+    $('#js-mockup-slide').addClass('hide').removeClass('show')
+    $('#js-tools-slide').addClass('hide').removeClass('show')
 })
 
-$('#wireframe').click(function() {
-	$('#wireframe-slider, #mockup-slider, #tools-slider').hide()
-	$('#wireframe-slider').show();
-});
+$('#mockup').click(function(){
+    $('#js-mockup-slide').addClass('show').removeClass('hide')
+    $('#js-wireframe-slide').addClass('hide').removeClass('show')
+    $('#js-tools-slide').addClass('hide').removeClass('show')
+})
 
-$('#mockup').click(function() {
-	$('#wireframe-slider, #mockup-slider, #tools-slider').hide();
-	$('#mockup-slider').show();
-});
-
-$('#tools').click(function() {
-	$('#wireframe-slider, #mockup-slider, #tools-slider').hide();
-	$('#tools-slider').show();
-});
+$('#tools').click(function(){
+    $('#js-tools-slide').addClass('show').removeClass('hide')
+    $('#js-wireframe-slide').addClass('hide').removeClass('show')
+    $('#js-mockup-slide').addClass('hide').removeClass('show')
+})
